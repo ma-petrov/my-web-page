312faaa776f2cf3022ac517bbbfa6a1197985b68
Petrov Mikhail <petrov.ma@icloud.com>
2022-09-28 20:10:02 +0300

    add email

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,8 +8,8 @@ fi
 domains=(clipdecliprepeat.ru www.clipdecliprepeat.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="petrov.ma@icloud.com"
+staging=0
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
