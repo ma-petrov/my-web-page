599a6667d792ad8777368957651c07e64aab1beb
Petrov Mikhail <petrov.ma@icloud.com>
2022-10-09 05:38:08 +0300

    add scrolling

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,12 +1,71 @@
+const ABOUT_ME = 1;
+const CV = 2;
+
 const aboutMe = document.getElementById("about-me");
 const cv = document.getElementById("cv");
 
-document.getElementById("open-cv-button").addEventListener("click", () => {
-    aboutMe.style.top = "-100vh";
-    cv.style.top = "0vh";
-});
+let currentPage = ABOUT_ME;
+let ticking = false;
+let lastKnownScrollPosition = 0;
 
-document.getElementById("close-cv-button").addEventListener("click", () => {
-    aboutMe.style.top = "0vh";
-    cv.style.top = "100vh";
-});
+function openCvPage() {
+    if (currentPage == ABOUT_ME) {
+        aboutMe.style.top = "-100vh";
+        cv.style.top = "0vh";
+        currentPage = CV;
+    }
+}
+
+function closeCvPage() {
+    if (currentPage == CV) {
+        aboutMe.style.top = "0vh";
+        cv.style.top = "100vh";
+        currentPage = ABOUT_ME;
+    }
+}
+
+function switchPage(scrollDelta) {
+    if (scrollDelta < 0) {
+        openCvPage();
+    }
+    else {
+        closeCvPage();
+    }
+}
+
+function scroll(deltaY) {
+    if (!ticking) {
+        window.requestAnimationFrame(() => {
+            switchPage(deltaY);
+            ticking = false;
+        });
+        ticking = true;
+    }    
+}
+
+document.getElementById("open-cv-button").addEventListener("click", openCvPage);
+document.getElementById("close-cv-button").addEventListener("click", closeCvPage);
+document.addEventListener("wheel", (e) => {scroll(e.wheelDeltaY)});
+document.addEventListener("scroll", (e) => {scroll(e.deltaY)});
+
+// document.addEventListener("scroll", () => {
+//     lastKnownScrollPosition = window.scrollY;
+//     if (!ticking) {
+//         window.requestAnimationFrame(() => {
+//             console.log(`aboutMe scroll position: ${lastKnownScrollPosition}`);
+//             setTimeout(() => {ticking = false;}, TIMEOUT);
+//         });
+//         ticking = true;
+//     }
+// });
+
+// document.addEventListener("scroll", () => {
+//     lastKnownScrollPosition = window.scrollY;
+//     if (!ticking) {
+//         window.requestAnimationFrame(() => {
+//             console.log(`cv scroll position: ${lastKnownScrollPosition}`);
+//             setTimeout(() => {ticking = false;}, TIMEOUT);
+//         });
+//         ticking = true;
+//     }
+// });
