5f20cb5c4473722366765b81c94ed46d37d2e1a0
Petrov Mikhail <petrov.ma@icloud.com>
2022-12-02 00:00:07 +0300

    base

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,16 +1,16 @@
 const ABOUT_ME = 0;
 const CV = 1;
 
+const about_me = document.querySelector('#about-me');
+const cv = document.querySelector('#cv');
 
 let prevScrollTop = window.pageYOffset;
 let containerId = 0;
-
 
 function setScrollTop(containerId) {
     prevScrollTop = document.documentElement.clientHeight * containerId;
     window.scroll({top: prevScrollTop, behavior: "smooth"});
 }
-
 
 function scrollPage() {
     const threshold = Math.floor(document.documentElement.clientHeight/5);
@@ -43,5 +43,18 @@ document.addEventListener("scroll", () => {
 
 document.addEventListener("scrollend", scrollPage);
 document.addEventListener("touchend", scrollPage);
-// document.getElementById("open-aboutme-button").addEventListener("click", () => {if (containerId != ABOUT_ME) {setScrollTop(ABOUT_ME);}});
-// document.getElementById("open-cv-button").addEventListener("click", () => {if (containerId != CV) {setScrollTop(CV);}});
+document.querySelector("#open-aboutme-button").addEventListener("click", () => {
+    if (containerId != ABOUT_ME) {
+        containerId = ABOUT_ME;
+        about_me.style.top = "0px";
+        cv.style.top = "100vh";
+    }
+});
+
+document.querySelector("#open-cv-button").addEventListener("click", () => {
+    if (containerId != CV) {
+        containerId = CV;
+        about_me.style.top = "-100vh";
+        cv.style.top = "0px";
+    }
+});
